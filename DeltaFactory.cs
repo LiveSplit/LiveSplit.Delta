ad406f0545b07413c87bbd02ce8c9edd7a646778
CryZe <cryze92@gmail.com>
2014-07-13 02:08:38 +0200

    Initial Commit

---
+++ DeltaFactory.cs
@@ -0,0 +1,64 @@
+﻿using LiveSplit.Delta;
+using LiveSplit.Model;
+using LiveSplit.UI.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: ComponentFactory(typeof(DeltaFactory))]
+
+namespace LiveSplit.Delta
+{
+    public class DeltaFactory : IComponentFactory
+    {
+        public string ComponentName
+        {
+            get { return "Delta"; }
+        }
+
+        public string Description
+        {
+            get { return ""; }
+        }
+
+        public ComponentCategory Category
+        {
+            get { return ComponentCategory.Information; }
+        }
+
+        public IComponent Create(LiveSplitState state)
+        {
+            return new DeltaComponent(state);
+        }
+
+        public string UpdateName
+        {
+            get { return ComponentName; }
+        }
+
+        public string XMLURL
+        {
+#if RELEASE_CANDIDATE
+            get { return "http://livesplit.org/update_rc_sdhjdop/Components/update.LiveSplit.DeltaComponent.xml"; }
+#else
+            get { return "http://livesplit.org/update/Components/update.LiveSplit.DeltaComponent.xml"; }
+#endif
+        }
+
+        public string UpdateURL
+        {
+#if RELEASE_CANDIDATE
+            get { return "http://livesplit.org/update_rc_sdhjdop/"; }
+#else
+            get { return "http://livesplit.org/update/"; }
+#endif
+        }
+
+        public Version Version
+        {
+            get { return Version.Parse("1.0.6"); }
+        }
+    }
+}
