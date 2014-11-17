b6e9724da8f391066a2b2090269c16e6390854d3
wooferzfg1 <spapushin@gmail.com>
2014-11-17 00:13:06 -0600

    Removed RenameComparison

--- UI/Components/DeltaComponent.cs
+++ UI/Components/DeltaComponent.cs
@@ -40,6 +40,14 @@ namespace LiveSplit.UI.Components
             Formatter = new DeltaComponentFormatter(Settings.Accuracy, Settings.DropDecimals);
             InternalComponent = new InfoTimeComponent(null, null, Formatter);
             Cache = new GraphicsCache();
+            state.ComparisonRenamed += state_ComparisonRenamed;
+        }
+
+        void state_ComparisonRenamed(object sender, EventArgs e)
+        {
+            var args = (RenameEventArgs)e;
+            if (Settings.Comparison == args.OldName)
+                Settings.Comparison = args.NewName;
         }
 
         private void PrepareDraw(LiveSplitState state)
@@ -129,14 +137,6 @@ namespace LiveSplit.UI.Components
         {
             return Settings.GetSettings(document);
         }
-
-
-        public void RenameComparison(string oldName, string newName)
-        {
-            if (Settings.Comparison == oldName)
-                Settings.Comparison = newName;
-        }
-
 
         public void Update(IInvalidator invalidator, LiveSplitState state, float width, float height, LayoutMode mode)
         {
