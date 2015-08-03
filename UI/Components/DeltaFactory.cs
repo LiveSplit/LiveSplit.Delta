abe04d91ec9ba214a4400552c5cdd4bc51b13c8b
Lioncash <mathew1800@gmail.com>
2015-08-02 20:50:47 -0400

    Remove unused project references and directives

--- UI/Components/DeltaFactory.cs
+++ UI/Components/DeltaFactory.cs
@@ -2,10 +2,6 @@
 using LiveSplit.Model;
 using LiveSplit.UI.Components;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 [assembly: ComponentFactory(typeof(DeltaFactory))]
 
