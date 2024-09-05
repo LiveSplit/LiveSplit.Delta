c7edd023c17c30752c987ba25e80e3eb402ba87a
Ero <56401411+just-ero@users.noreply.github.com>
2024-09-05 03:42:11 +0200

    Add `.editorconfig` & apply style rules (#12)
    * Add `.editorconfig`
    
    * Upgrade to C# 12
    
    * Use file-scoped namespaces, move `using` directives, fix whitespace
    
    * `IDE0005`, `IDE0065`, `IDE0161`
    
    * Add and order modifiers
    
    * `IDE0036`, `IDE0040`, `IDE0044`, `IDE0064`, `IDE0250`, `IDE0251`
    
    * Apply member body preferences
    
    * `IDE0021`, `IDE0022`, `IDE0023`, `IDE0024`, `IDE0025`, `IDE0026`, `IDE0027`, `IDE0053`, `IDE0061`
    
    * Apply braces, parentheses, new-line preferences
    
    * `IDE0011`, `IDE0047`, `IDE0048`, `IDE2000`, `IDE2001`, `IDE2002`, `IDE2003`, `IDE2004`, `IDE2005`, `IDE2006`
    
    * Use pattern matching
    
    * `IDE0019`, `IDE0020`, `IDE0038`, `IDE0078`, `IDE0083`, `IDE0170`, `IDE0260`
    
    * Apply `var` preferences, simplify `new` expression
    
    * `IDE0007`, `IDE0008`, `IDE0090`
    
    * Use index/range operator
    
    * `IDE0056`, `IDE0057`
    
    * Enforce code style in build

--- src/LiveSplit.Delta/UI/Components/DeltaComponent.cs
+++ src/LiveSplit.Delta/UI/Components/DeltaComponent.cs
@@ -1,196 +1,207 @@
-﻿using LiveSplit.Model;
-using LiveSplit.Model.Comparisons;
-using LiveSplit.TimeFormatters;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Windows.Forms;
 
-namespace LiveSplit.UI.Components
+using LiveSplit.Model;
+using LiveSplit.Model.Comparisons;
+using LiveSplit.TimeFormatters;
+
+namespace LiveSplit.UI.Components;
+
+public class DeltaComponent : IComponent
 {
-    public class DeltaComponent : IComponent
+    protected InfoTimeComponent InternalComponent { get; set; }
+    public DeltaSettings Settings { get; set; }
+    private GeneralTimeFormatter Formatter { get; set; }
+
+    public float PaddingTop => InternalComponent.PaddingTop;
+    public float PaddingLeft => InternalComponent.PaddingLeft;
+    public float PaddingBottom => InternalComponent.PaddingBottom;
+    public float PaddingRight => InternalComponent.PaddingRight;
+
+    public IDictionary<string, Action> ContextMenuControls => null;
+
+    public DeltaComponent(LiveSplitState state)
     {
-        protected InfoTimeComponent InternalComponent { get; set; }
-        public DeltaSettings Settings { get; set; }
-        private GeneralTimeFormatter Formatter { get; set; }
-
-        public float PaddingTop => InternalComponent.PaddingTop;
-        public float PaddingLeft => InternalComponent.PaddingLeft;
-        public float PaddingBottom => InternalComponent.PaddingBottom;
-        public float PaddingRight => InternalComponent.PaddingRight;
-
-        public IDictionary<string, Action> ContextMenuControls => null;
-
-        public DeltaComponent(LiveSplitState state)
+        Settings = new DeltaSettings()
         {
-            Settings = new DeltaSettings()
-            {
-                CurrentState = state
-            };
-            Formatter = new GeneralTimeFormatter() 
-            {
-                NullFormat = NullFormat.Dash,
-                Accuracy = Settings.Accuracy,
-                DropDecimals = Settings.DropDecimals,
-            };
-            InternalComponent = new InfoTimeComponent(null, null, Formatter);
-            state.ComparisonRenamed += state_ComparisonRenamed;
+            CurrentState = state
+        };
+        Formatter = new GeneralTimeFormatter()
+        {
+            NullFormat = NullFormat.Dash,
+            Accuracy = Settings.Accuracy,
+            DropDecimals = Settings.DropDecimals,
+        };
+        InternalComponent = new InfoTimeComponent(null, null, Formatter);
+        state.ComparisonRenamed += state_ComparisonRenamed;
+    }
+
+    private void state_ComparisonRenamed(object sender, EventArgs e)
+    {
+        var args = (RenameEventArgs)e;
+        if (Settings.Comparison == args.OldName)
+        {
+            Settings.Comparison = args.NewName;
+            ((LiveSplitState)sender).Layout.HasChanged = true;
+        }
+    }
+
+    private void PrepareDraw(LiveSplitState state)
+    {
+        InternalComponent.DisplayTwoRows = Settings.Display2Rows;
+
+        InternalComponent.NameLabel.HasShadow
+            = InternalComponent.ValueLabel.HasShadow
+            = state.LayoutSettings.DropShadows;
+
+        Formatter.Accuracy = Settings.Accuracy;
+        Formatter.DropDecimals = Settings.DropDecimals;
+
+        InternalComponent.NameLabel.ForeColor = Settings.OverrideTextColor ? Settings.TextColor : state.LayoutSettings.TextColor;
+    }
+
+    private void DrawBackground(Graphics g, LiveSplitState state, float width, float height)
+    {
+        if (Settings.BackgroundColor.A > 0
+            || (Settings.BackgroundGradient != GradientType.Plain
+            && Settings.BackgroundColor2.A > 0))
+        {
+            var gradientBrush = new LinearGradientBrush(
+                        new PointF(0, 0),
+                        Settings.BackgroundGradient == GradientType.Horizontal
+                        ? new PointF(width, 0)
+                        : new PointF(0, height),
+                        Settings.BackgroundColor,
+                        Settings.BackgroundGradient == GradientType.Plain
+                        ? Settings.BackgroundColor
+                        : Settings.BackgroundColor2);
+            g.FillRectangle(gradientBrush, 0, 0, width, height);
+        }
+    }
+
+    public void DrawVertical(Graphics g, LiveSplitState state, float width, Region clipRegion)
+    {
+        DrawBackground(g, state, width, VerticalHeight);
+        PrepareDraw(state);
+        InternalComponent.DrawVertical(g, state, width, clipRegion);
+    }
+
+    public void DrawHorizontal(Graphics g, LiveSplitState state, float height, Region clipRegion)
+    {
+        DrawBackground(g, state, HorizontalWidth, height);
+        PrepareDraw(state);
+        InternalComponent.DrawHorizontal(g, state, height, clipRegion);
+    }
+
+    public float VerticalHeight => InternalComponent.VerticalHeight;
+
+    public float MinimumWidth => InternalComponent.MinimumWidth;
+
+    public float HorizontalWidth => InternalComponent.HorizontalWidth;
+
+    public float MinimumHeight => InternalComponent.MinimumHeight;
+
+    public string ComponentName
+        => "Delta" + (Settings.Comparison == "Current Comparison"
+            ? ""
+            : " (" + CompositeComparisons.GetShortComparisonName(Settings.Comparison) + ")");
+
+    public Control GetSettingsControl(LayoutMode mode)
+    {
+        Settings.Mode = mode;
+        return Settings;
+    }
+
+    public void SetSettings(System.Xml.XmlNode settings)
+    {
+        Settings.SetSettings(settings);
+    }
+
+    public System.Xml.XmlNode GetSettings(System.Xml.XmlDocument document)
+    {
+        return Settings.GetSettings(document);
+    }
+
+    public void Update(IInvalidator invalidator, LiveSplitState state, float width, float height, LayoutMode mode)
+    {
+        string comparison = Settings.Comparison == "Current Comparison" ? state.CurrentComparison : Settings.Comparison;
+        if (!state.Run.Comparisons.Contains(comparison))
+        {
+            comparison = state.CurrentComparison;
         }
 
-        void state_ComparisonRenamed(object sender, EventArgs e)
+        string comparisonName = comparison.StartsWith("[Race] ") ? comparison[7..] : comparison;
+
+        bool useLiveDelta = false;
+        if (state.CurrentPhase is TimerPhase.Running or TimerPhase.Paused)
         {
-            var args = (RenameEventArgs)e;
-            if (Settings.Comparison == args.OldName)
+            TimeSpan? delta = LiveSplitStateHelper.GetLastDelta(state, state.CurrentSplitIndex, comparison, state.CurrentTimingMethod);
+            TimeSpan? liveDelta = state.CurrentTime[state.CurrentTimingMethod] - state.CurrentSplit.Comparisons[comparison][state.CurrentTimingMethod];
+            if (liveDelta > delta || (delta == null && liveDelta > TimeSpan.Zero))
             {
-                Settings.Comparison = args.NewName;
-                ((LiveSplitState)sender).Layout.HasChanged = true;
+                delta = liveDelta;
+                useLiveDelta = true;
             }
+
+            InternalComponent.TimeValue = delta;
+        }
+        else if (state.CurrentPhase == TimerPhase.Ended)
+        {
+            InternalComponent.TimeValue = state.Run.Last().SplitTime[state.CurrentTimingMethod] - state.Run.Last().Comparisons[comparison][state.CurrentTimingMethod];
+        }
+        else
+        {
+            InternalComponent.TimeValue = null;
         }
 
-        private void PrepareDraw(LiveSplitState state)
+        string text = comparisonName;
+        if (Settings.OverrideText)
         {
-            InternalComponent.DisplayTwoRows = Settings.Display2Rows;
-
-            InternalComponent.NameLabel.HasShadow 
-                = InternalComponent.ValueLabel.HasShadow
-                = state.LayoutSettings.DropShadows;
-
-            Formatter.Accuracy = Settings.Accuracy;
-            Formatter.DropDecimals = Settings.DropDecimals;
-
-            InternalComponent.NameLabel.ForeColor = Settings.OverrideTextColor ? Settings.TextColor : state.LayoutSettings.TextColor;
-        }
-
-        private void DrawBackground(Graphics g, LiveSplitState state, float width, float height)
-        {
-            if (Settings.BackgroundColor.A > 0
-                || Settings.BackgroundGradient != GradientType.Plain
-                && Settings.BackgroundColor2.A > 0)
+            InternalComponent.AlternateNameText.Clear();
+            if (Settings.DifferentialText)
             {
-                var gradientBrush = new LinearGradientBrush(
-                            new PointF(0, 0),
-                            Settings.BackgroundGradient == GradientType.Horizontal
-                            ? new PointF(width, 0)
-                            : new PointF(0, height),
-                            Settings.BackgroundColor,
-                            Settings.BackgroundGradient == GradientType.Plain
-                            ? Settings.BackgroundColor
-                            : Settings.BackgroundColor2);
-                g.FillRectangle(gradientBrush, 0, 0, width, height);
-            }
-        }
-
-        public void DrawVertical(Graphics g, LiveSplitState state, float width, Region clipRegion)
-        {
-            DrawBackground(g, state, width, VerticalHeight);
-            PrepareDraw(state);
-            InternalComponent.DrawVertical(g, state, width, clipRegion);
-        }
-
-        public void DrawHorizontal(Graphics g, LiveSplitState state, float height, Region clipRegion)
-        {
-            DrawBackground(g, state, HorizontalWidth, height);
-            PrepareDraw(state);
-            InternalComponent.DrawHorizontal(g, state, height, clipRegion);
-        }
-
-        public float VerticalHeight => InternalComponent.VerticalHeight;
-
-        public float MinimumWidth => InternalComponent.MinimumWidth;
-
-        public float HorizontalWidth => InternalComponent.HorizontalWidth;
-
-        public float MinimumHeight => InternalComponent.MinimumHeight;
-
-        public string ComponentName
-            => "Delta" + (Settings.Comparison == "Current Comparison" 
-                ? "" 
-                : " (" + CompositeComparisons.GetShortComparisonName(Settings.Comparison) + ")"); 
-
-        public Control GetSettingsControl(LayoutMode mode)
-        {
-            Settings.Mode = mode;
-            return Settings;
-        }
-
-        public void SetSettings(System.Xml.XmlNode settings)
-        {
-            Settings.SetSettings(settings);
-        }
-
-        public System.Xml.XmlNode GetSettings(System.Xml.XmlDocument document)
-        {
-            return Settings.GetSettings(document);
-        }
-
-        public void Update(IInvalidator invalidator, LiveSplitState state, float width, float height, LayoutMode mode)
-        {
-            var comparison = Settings.Comparison == "Current Comparison" ? state.CurrentComparison : Settings.Comparison;
-            if (!state.Run.Comparisons.Contains(comparison))
-                comparison = state.CurrentComparison;
-            var comparisonName = comparison.StartsWith("[Race] ") ? comparison.Substring(7) : comparison;
-
-            var useLiveDelta = false;
-            if (state.CurrentPhase == TimerPhase.Running || state.CurrentPhase == TimerPhase.Paused)
-            {
-                TimeSpan? delta = LiveSplitStateHelper.GetLastDelta(state, state.CurrentSplitIndex, comparison, state.CurrentTimingMethod);
-                var liveDelta = state.CurrentTime[state.CurrentTimingMethod] - state.CurrentSplit.Comparisons[comparison][state.CurrentTimingMethod];
-                if (liveDelta > delta || (delta == null && liveDelta > TimeSpan.Zero))
-                {
-                    delta = liveDelta;
-                    useLiveDelta = true;
-                }
-                InternalComponent.TimeValue = delta;
-            }
-            else if (state.CurrentPhase == TimerPhase.Ended)
-            {
-                InternalComponent.TimeValue = state.Run.Last().SplitTime[state.CurrentTimingMethod] - state.Run.Last().Comparisons[comparison][state.CurrentTimingMethod];
+                text = InternalComponent.TimeValue < TimeSpan.Zero ? Settings.CustomTextAhead : Settings.CustomText;
+                InternalComponent.LongestString = Settings.CustomText.Length < Settings.CustomTextAhead.Length ? Settings.CustomTextAhead : Settings.CustomText;
             }
             else
             {
-                InternalComponent.TimeValue = null;
+                text = Settings.CustomText;
+                InternalComponent.LongestString = text;
             }
-
-            var text = comparisonName;
-            if (Settings.OverrideText)
+        }
+        else
+        {
+            InternalComponent.LongestString = text;
+            if (InternalComponent.InformationName != text)
             {
                 InternalComponent.AlternateNameText.Clear();
-                if (Settings.DifferentialText)
-                {
-                    text = InternalComponent.TimeValue < TimeSpan.Zero ? Settings.CustomTextAhead : Settings.CustomText;
-                    InternalComponent.LongestString = Settings.CustomText.Length < Settings.CustomTextAhead.Length ? Settings.CustomTextAhead : Settings.CustomText;
-                }
-                else
-                {
-                    text = Settings.CustomText;
-                    InternalComponent.LongestString = text;
-                }
+                InternalComponent.AlternateNameText.Add(CompositeComparisons.GetShortComparisonName(comparison));
             }
-            else
-            {
-                InternalComponent.LongestString = text;
-                if (InternalComponent.InformationName != text)
-                {
-                    InternalComponent.AlternateNameText.Clear();
-                    InternalComponent.AlternateNameText.Add(CompositeComparisons.GetShortComparisonName(comparison));
-                }
-            }
-            InternalComponent.InformationName = text;
-
-            var color = LiveSplitStateHelper.GetSplitColor(state, InternalComponent.TimeValue, state.CurrentSplitIndex - (useLiveDelta ? 0 : 1), true, false, comparison, state.CurrentTimingMethod);
-            if (color == null)
-                color = Settings.OverrideTextColor ? Settings.TextColor : state.LayoutSettings.TextColor;
-            InternalComponent.ValueLabel.ForeColor = color.Value;
-
-            InternalComponent.Update(invalidator, state, width, height, mode);
         }
 
-        public void Dispose()
+        InternalComponent.InformationName = text;
+
+        Color? color = LiveSplitStateHelper.GetSplitColor(state, InternalComponent.TimeValue, state.CurrentSplitIndex - (useLiveDelta ? 0 : 1), true, false, comparison, state.CurrentTimingMethod);
+        if (color == null)
         {
+            color = Settings.OverrideTextColor ? Settings.TextColor : state.LayoutSettings.TextColor;
         }
 
-        public int GetSettingsHashCode() => Settings.GetSettingsHashCode();
+        InternalComponent.ValueLabel.ForeColor = color.Value;
+
+        InternalComponent.Update(invalidator, state, width, height, mode);
+    }
+
+    public void Dispose()
+    {
+    }
+
+    public int GetSettingsHashCode()
+    {
+        return Settings.GetSettingsHashCode();
     }
 }
